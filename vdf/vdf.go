52f2953cda9de1e5ef6156211c9ec6c428daf2b7
Jing Leng <jingleng@cs.nyu.edu>
2018-04-03 17:02:04 -0400

    tidy up

--- vdf/vdf.go
+++ vdf/vdf.go
@@ -12,12 +12,12 @@ import (
 
 const t = 10000  // 1000000
 const B = 100000 // 1 << 10
-const lambda = 1000
+const lambda = 10
 
 type EvalKey struct {
 	G  *big.Int
 	H  func(*big.Int) *big.Int
-	gs []*big.Int
+	Gs []*big.Int
 }
 
 type VerifyKey struct {
@@ -68,49 +68,41 @@ func isStrongPrime(prime *big.Int) bool {
 // interface
 func Setup() (*EvalKey, *VerifyKey) {
 	fmt.Println("setup")
-	fmt.Printf("parameters: t: %v, B: %v\n", t, B)
+	fmt.Printf("parameters: t: %v, B: %v, lambda: %v \n", t, B, lambda)
 
 	L := computeL(t)
-	fmt.Println("Number of primes in L:", len(L))
-	fmt.Printf("L %v ... \n", L[:5])
+	fmt.Printf("L [%v %v %v %v ... %v %v %v] \n", L[0], L[1], L[2], L[3], L[len(L)-3], L[len(L)-2], L[len(L)-1])
 	P := big.NewInt(1)
-	for i, v := range L {
-		fmt.Println(i)
+	for _, v := range L {
 		P.Mul(P, v)
 	}
-	fmt.Println("P", P)
 
-	// var privateKey *rsa.PrivateKey
 	p := big.NewInt(1)
 	q := big.NewInt(1)
-	var privateKey *rsa.PrivateKey
+	var rsaKey *rsa.PrivateKey
 	var N *big.Int
 
 	for !isStrongPrime(p) || !isStrongPrime(q) {
-		privateKey, _ = rsa.GenerateKey(cryptorand.Reader, 59)
-		//if err != nil {
-		//	fmt.Println(err)
-		//}
-		N = privateKey.PublicKey.N
-		p = privateKey.Primes[0]
-		q = privateKey.Primes[1]
+		rsaKey, _ = rsa.GenerateKey(cryptorand.Reader, 59)
+		N = rsaKey.PublicKey.N
+		p = rsaKey.Primes[0]
+		q = rsaKey.Primes[1]
 	}
 
-	fmt.Println("p and q", privateKey.Primes)
-	fmt.Println("N and e", privateKey.PublicKey)
-	fmt.Println("D", privateKey.D)
-	fmt.Println("precomputed", privateKey.Precomputed)
+	fmt.Println("p and q", rsaKey.Primes)
+	fmt.Println("N and e", rsaKey.PublicKey)
+	fmt.Println("D", rsaKey.D)
+	fmt.Println("precomputed", rsaKey.Precomputed)
 	tmp := big.NewInt(0)
-	tmp.Add(privateKey.Primes[0], big.NewInt(-1))
+	tmp.Add(rsaKey.Primes[0], big.NewInt(-1))
 	phi := big.NewInt(0)
-	phi.Add(privateKey.Primes[1], big.NewInt(-1))
+	phi.Add(rsaKey.Primes[1], big.NewInt(-1))
 	phi.Mul(phi, tmp)
 	fmt.Println("phi", phi)
 
-	hashfunc := func(input *big.Int) (hashval *big.Int) { // TODO: change hash function to make sure prime doesn't divide phi
+	hashfunc := func(input *big.Int) (hashval *big.Int) {
 		h := sha256.New()
 		h.Write([]byte(input.Bytes()))
-		// fmt.Printf("%v", h.Sum(nil))
 		shasum := h.Sum(nil)
 		hashval = big.NewInt(0)
 		hashval.SetBytes(shasum)
@@ -119,20 +111,18 @@ func Setup() (*EvalKey, *VerifyKey) {
 	}
 	P_inv := big.NewInt(1)
 	P_inv.ModInverse(P, phi)
-	fmt.Println("P_inv", P_inv)
 
 	hs := computehs(hashfunc, B)
 	gs := computegs(hs, P_inv, N)
 
 	evaluateKey := EvalKey{N, hashfunc, gs}
 	verifyKey := VerifyKey{N, hashfunc}
-	// fmt.Println("evaluate key", evaluateKey)
 	return &evaluateKey, &verifyKey
 }
 
 func Evaluate(evaluateKey *EvalKey, x int) (y *big.Int) {
 	N := evaluateKey.G
-	gs := evaluateKey.gs
+	gs := evaluateKey.Gs
 	L := computeL(t)
 	rand.Seed(int64(x))
 	fmt.Println("evaluate")
@@ -154,19 +144,13 @@ func Evaluate(evaluateKey *EvalKey, x int) (y *big.Int) {
 		g_x.Mod(g_x, N)
 	}
 
-	fmt.Println("L_x", L_x)
-	fmt.Println("S_x", S_x)
-
 	P := big.NewInt(1)
 	for _, v := range L {
 		P.Mul(P, v)
 	}
-	fmt.Println("P", P)
-	fmt.Println("P_x", P_x)
 
 	exp_coeff := big.NewInt(1)
 	exp_coeff.Div(P, P_x)
-	fmt.Println("P / Px = ", exp_coeff)
 
 	fmt.Println("g_x", g_x)
 
@@ -174,10 +158,6 @@ func Evaluate(evaluateKey *EvalKey, x int) (y *big.Int) {
 	y.Exp(g_x, exp_coeff, N)
 
 	fmt.Println("y", y)
-	return
-}
-
-func shortcut(x int) (y *big.Int) {
 	return
 }
 
@@ -206,8 +186,6 @@ func Verify(verifyKey *VerifyKey, y *big.Int, x int) bool {
 		h_x.Mul(h_x, hs[v])
 		h_x.Mod(h_x, N)
 	}
-	fmt.Println("L_x", L_x)
-	fmt.Println("S_x", S_x)
 	h2 := big.NewInt(1)
 
 	h2.Exp(y, P_x, N)
